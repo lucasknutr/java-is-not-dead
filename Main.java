14f3d9ec3181ca842ed04935a6c5ebf06837cf5d
Lucas <lucasclaudinocc@gmail.com>
2023-11-07 08:46:22 -0300

    Learning inheritance and other OOP fundamentals

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static int Add(int a, int b) {
         return a + b;
     }
-    public static double generateRandom () {
-        return Math.floor(Math.random() * 255.999);
-    }
+    public static double generateRandom () { return Math.floor(Math.random() * 255.999); }
     public static void getRGBValues() {
         double r = generateRandom();
         double g = generateRandom();
@@ -14,10 +14,41 @@ public class Main {
         System.out.println("r: " + r + " g: " + g + " b: " + b);
     }
 
-    public static String getName() {
-        System.out.println("What is your name?");
+    private static List<Cake> cakes = new ArrayList<>();
+
+    public static Cake getCakeByName(String name) {
+        for (Cake cake : cakes) {
+            if (cake.name.equals(name)) {
+                return cake;
+            }
+        }
+        return null;
+    }
+
+    public static String compareCakePrices(Cake firstCake, Cake secondCake) {
+
+        if (firstCake.price == secondCake.price) {
+            return String.format("The cakes %s and %s have the same price. They both cost %s !", firstCake.name, secondCake.name, firstCake.price);
+        } else if (firstCake.price > secondCake.price) {
+            return String.format("The cake %s is more expensive than the cake %s. The cake %s costs %s and the cake %s costs %s.", firstCake.name, secondCake.name, firstCake.name, firstCake.price, secondCake.name, secondCake.price);
+        } else {
+            return String.format("The cake %s is more expensive than the cake %s. The cake %s costs %s and the cake %s costs %s.", secondCake.name, firstCake.name, secondCake.name, secondCake.price, firstCake.name, firstCake.price);
+        }
+    }
+
+    public static void getCakes () {
         Scanner scanner = new Scanner(System.in);
-        return scanner.nextLine();
+        System.out.println("Enter the first cake name: ");
+        String firstCakeName = scanner.nextLine();
+        System.out.println("Enter the second cake name: ");
+        String secondCakeName = scanner.nextLine();
+        scanner.close();
+        Cake firstCake = getCakeByName(firstCakeName);
+        Cake secondCake = getCakeByName(secondCakeName);
+
+        String comparisonResult = compareCakePrices(firstCake, secondCake);
+        System.out.println(comparisonResult);
+
     }
 
     // MAIN APP
@@ -25,6 +56,9 @@ public class Main {
         Cake bombocado = new Cake("Bombocado", 22, 12, true);
         Cake boloDeCenoura = new Cake("Cenoura", 20, 11, true);
         Cake boloDeChocolate = new Cake("Chocolate", 18, 10, true);
-
+        cakes.add(bombocado);
+        cakes.add(boloDeCenoura);
+        cakes.add(boloDeChocolate);
+        getCakes();
     }
 }
